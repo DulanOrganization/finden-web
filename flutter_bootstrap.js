10d31732083635d6762b7e2e796f83584bb23184
JasmitSingh90 <jasmitsingh90@gmail.com>
2025-03-05 19:33:10 +0530

    Updated the version to 3.1.2+4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3728157314"
+    serviceWorkerVersion: "1783387338"
   }
 });
